968d1baeb6b89a1c17d462e9913ba9408998e36e
saniya-farheen <saniyafarheen188@gmail.com>
2026-03-28 23:04:18 +0530

    small commit

--- titanic_manual.py
+++ titanic_manual.py
@@ -3,7 +3,13 @@ import pandas as pd
 # directly reads from internet!
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
 
-# save to your computer
+
 df.to_csv("titanic.csv", index=False)
 print("Downloaded!")
 print(df.head())
+print(df.info())        
+print(df.isnull().sum()) 
+
+with open("titanic.csv", "r") as file:
+    frst_line = file.readline()
+    print(frst_line)
