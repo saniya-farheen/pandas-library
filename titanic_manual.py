a0595ebfbd8609d8a1bdf31ea350ecaea8eaf53c
saniya-farheen <saniyafarheen188@gmail.com>
2026-03-29 23:43:30 +0530

    titani

--- titanic_manual.py
+++ titanic_manual.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # directly reads from internet!
-df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+
 
 
 df.to_csv("titanic.csv", index=False)
